8e6ad246db68fc1f16ddc4738803043d9697bf9a
quimarche <chris@quimarche.co.uk>
2022-08-07 12:18:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quimarche-vue-starter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-fny17p)
---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>App</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('App mounted');
+  },
+};
+</script>
+
+<style lang="sass">
+body
+  background: WhiteSmoke
+</style>
